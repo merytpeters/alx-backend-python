2aa35908080d4828582523b4cdc164e3e229fdf4
merytpeters <merytpeters@gmail.com>
2024-10-10 20:31:35 +0100

    Takes in a float and return as string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+"""Type Annotated function to_str that
+takes float as argument and returns string
+"""
+
+
+def to_str(n: float) -> str:
+    """Function that takes float as argument but
+    returns str"""
+    return str(n)
