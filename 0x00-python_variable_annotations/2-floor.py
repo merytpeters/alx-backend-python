6a65a7632eca2b174e6e9be34e1cdea92efbab90
merytpeters <merytpeters@gmail.com>
2024-10-10 20:23:54 +0100

    Type annotated floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Type-annotation function floor
+Takes in float argument and returns the
+floor of the float"""
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """Floor function"""
+    return math.floor(n)
