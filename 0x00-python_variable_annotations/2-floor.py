9bd97bc933bf0bdaf97c3255e9c5075ed4876697
merytpeters <merytpeters@gmail.com>
2024-10-10 20:27:08 +0100

    Updated to return floor as int

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -2,11 +2,9 @@
 """Type-annotation function floor
 Takes in float argument and returns the
 floor of the float"""
-
-
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """Floor function"""
     return math.floor(n)
