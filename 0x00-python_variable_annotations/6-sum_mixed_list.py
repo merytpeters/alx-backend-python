3888242da98d28b195b26ed980bc3a38bfe341a9
merytpeters <merytpeters@gmail.com>
2024-10-10 21:04:02 +0100

    Sum of mixed list of integers and floats, int comes first

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,6 @@ and float
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns sum of integers and floats"""
     return sum(mxd_lst)
