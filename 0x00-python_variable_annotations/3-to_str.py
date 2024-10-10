623527761876af5dbd472546f21c7f92376f2b1e
merytpeters <merytpeters@gmail.com>
2024-10-10 20:35:59 +0100

    Takes in a float and return as string, shebang added

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """Type Annotated function to_str that
 takes float as argument and returns string
 """
@@ -5,5 +6,6 @@ takes float as argument and returns string
 
 def to_str(n: float) -> str:
     """Function that takes float as argument but
-    returns str"""
+    returns str
+    """
     return str(n)
