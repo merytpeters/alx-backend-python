ad237665789a5ff1789a9a394835e7cced8081d2
merytpeters <merytpeters@gmail.com>
2024-10-14 21:35:27 +0100

    Returns sorted List of all delays n times, without using sort()

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> float:
     """ Async function that spawns wait_random n times and returns
     sorted list of delays without using sort()"""
     tasks = [wait_random(max_delay) for _ in range(n)]
