30af52cd6237d76eb58ebe7591a8ffcd91957eda
merytpeters <merytpeters@gmail.com>
2024-10-14 21:29:06 +0100

    Returns sorted List of all delays n times, without using sort()

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    """ Async function that returns sorted list of all the delays"""
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    return sorted(delays)
+    """ Async function that spawns wait_random n times and returns
+    sorted list of delays without using sort()"""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return (delays)
