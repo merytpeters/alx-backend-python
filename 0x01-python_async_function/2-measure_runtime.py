009a10a03150d089695ef394c56aade52cd253ed
merytpeters <merytpeters@gmail.com>
2024-10-14 23:22:53 +0100

    Edges cases added

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,6 +9,10 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 async def measure_time(n: int, max_delay: int) -> float:
     """Using the time module to measure time elapsed and returns
     total time / n"""
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+    if max_delay < 0:
+        raise ValueError("maximum delay must be a positive integer")
 
     start_time = time.time()
     await wait_n(n, max_delay)
