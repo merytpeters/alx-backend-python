f5178287496319f818748a1ea2e028c9e4b2d19a
merytpeters <merytpeters@gmail.com>
2024-10-14 21:52:13 +0100

    Returns sorted List of all delays , trying to meet requirements

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,16 +2,25 @@
 """Async routine, random delay in ascending order"""
 
 
-import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """ Async function that spawns wait_random n times and returns
     sorted list of delays without using sort()"""
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
-    for task in asyncio.as_completed(tasks):
+    for task in tasks:
         delay = await task
-        delays.append(delay)
-    return (delays)
+        if not delays:
+            delays.append(delay)
+        else:
+            inserted = False
+            for i in range(len(delays)):
+                if delay < delays[i]:
+                    delays.insert(i, delay)
+                    inserted = True
+                    break
+            if not inserted:
+                delays.append(delay)
+    return delays
