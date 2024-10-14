1335eba38cbc3024929bcfcbc2088462124ab427
merytpeters <merytpeters@gmail.com>
2024-10-14 22:30:43 +0100

    Average time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Measures total execution time"""
+
+
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Using the time module to measure time elapsed and returns
+    total time / n"""
+
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    return total_time / n
