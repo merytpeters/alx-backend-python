f51d5e933c400a9318a51af2b9cbac553b3272e5
merytpeters <merytpeters@gmail.com>
2024-10-14 22:44:11 +0100

    Average time, trying to meet requirement

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -15,4 +15,4 @@ async def measure_time(n: int, max_delay: int) -> float:
     end_time = time.time()
     total_time = end_time - start_time
 
-    return total_time / n
+    return (total_time / n)
