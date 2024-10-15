b3d3837e24ebcadaa8015dc1dd23262a34857502
merytpeters <merytpeters@gmail.com>
2024-10-15 22:25:35 +0100

    Coroutine , asynchronously wait 1 sec and yield a random number between 0 to 10, trying to meet one requirement

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that loops 10 times, asynchronously wait 1second
     and yields a random number"""
     for _ in range(10):
